04e08677392cfdd331cc03be0c5adc59fed96cd2
pleons42 <pleons42@googlemail.com>
2026-03-27 00:19:31 +0100

    Delete icans-grading-pwa directory

--- icans-grading-pwa/sw.js
+++
@@ -1,5 +0,0 @@
-var CACHE='icans-grading-v1';
-var FILES=['./index.html','./manifest.json'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}))});
